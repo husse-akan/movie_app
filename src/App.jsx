d4a2baa42ecc7e32950397a5414a205c03954d38
husseakan <huseyinakan2002@gmail.com>
2024-10-16 14:36:20 +0200

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-// src/App.jsx
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./components/routes/HomePage";
-import MovieDetail from "./components/routes/FavoriteMovies";
+import FavoriteMovies from "./components/routes/FavoriteMovies"; // Se till att detta är rätt
+import MovieDetail from "./components/routes/MovieDetail";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/movie/:id" element={<MovieDetail />} />{" "}
-        {/* Lägg till routen för filmdetaljer */}
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/favorites" element={<FavoriteMovies />} />
       </Routes>
     </Layout>
   );
