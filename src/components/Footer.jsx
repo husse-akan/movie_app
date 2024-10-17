87c332f17aa43414e53ae0a040d3aeabdbdcc191
husseakan <huseyinakan2002@gmail.com>
2024-10-17 12:24:22 +0200

    done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer
+      className="py-8 text-center mt-14 bg-white border-t border-gray-100"
+      aria-label="Footer"
+    >
+      {/* Footer Text */}
+      <div className="text-gray-500 text-xs my-2 font-light">
+        <p>Movie App</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
